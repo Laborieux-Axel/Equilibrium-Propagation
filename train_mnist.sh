963f154410f8c06d412d328f02670782000bf2aa
Axel Laborieux <axel@goliath.c2n.u-psud.fr>
2020-04-19 14:08:14 +0200

    adding basic test lines

---
+++ train_mnist.sh
@@ -0,0 +1,1 @@
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --optim 'adam' --lrs 5e-4 5e-4 --epochs 2 --act 'mysig' --todo 'train' --betas 0.0 0.1 --T1 30 --T2 10 --check-thm --mbs 50 --device 0
