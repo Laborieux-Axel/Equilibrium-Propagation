14b75ac9b978554087b82aec6c16a6e85e3c4801
Axel Laborieux <axel@goliath.c2n.u-psud.fr>
2020-05-06 10:15:28 +0200

    update command line

--- check/gdu_check_mnist.sh
+++ check/gdu_check_mnist.sh
@@ -1,4 +1,4 @@
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --device 0
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'mse' --device 0
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'cel' --device 0
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'cel' --device 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'cel' --save --device 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'cel' --save --device 0
