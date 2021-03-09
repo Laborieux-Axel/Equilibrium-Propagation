93660ed6c5b0ec07978b674a69c169ce32e8cd5f
Axel Laborieux <axel@goliath.c2n.u-psud.fr>
2021-03-09 10:27:01 +0100

    updated checks

--- check/gdu_check_cifar10.sh
+++ check/gdu_check_cifar10.sh
@@ -1,7 +1,7 @@
 # mse, small beta
-python main.py --model 'CNN' --task 'CIFAR10' --act 'my_hard_sig' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.01 --T1 250 --T2 30 --loss 'mse' --save --device 0
+python main.py --model 'CNN' --task 'CIFAR10' --act 'hard_sigmoid' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.01 --T1 250 --T2 30 --loss 'mse' --save --device 7
 # mse, big beta
-python main.py --model 'CNN' --task 'CIFAR10' --act 'my_hard_sig' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.1 --T1 250 --T2 30 --loss 'mse' --save --device 0
+python main.py --model 'CNN' --task 'CIFAR10' --act 'hard_sigmoid' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.1 --T1 250 --T2 30 --loss 'mse' --save --device 7
 # mse, big beta symmetric
-python main.py --model 'CNN' --task 'CIFAR10' --act 'my_hard_sig' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.1 --T1 250 --T2 30 --loss 'mse' --save --device 0 --thirdphase
+python main.py --model 'CNN' --task 'CIFAR10' --act 'hard_sigmoid' --kernels 3 3 3 3 --channels 128 256 512 512 --pools 'mmmm' --strides 1 1 1 1 --paddings 1 1 1 0 --todo 'gducheck' --betas 0.0 0.1 --T1 250 --T2 30 --loss 'mse' --save --device 7 --thirdphase
 
--- check/gdu_check_mnist.sh
+++ check/gdu_check_mnist.sh
@@ -1,6 +1,6 @@
 # tanh, beta 0.01
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 0 --seed 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.01 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 7 --seed 0
 # tanh, beta 0.1
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 0 --seed 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --device 7 --seed 0
 # tanh, beta 0.1, and beta -0.1
-python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --thirdphase --device 0 --seed 0
+python main.py --model 'MLP' --task 'MNIST' --archi 784 512 10 --act 'tanh' --todo 'gducheck' --betas 0.0 0.1 --T1 30 --T2 10 --mbs 50 --loss 'mse' --save --thirdphase --device 7 --seed 0
